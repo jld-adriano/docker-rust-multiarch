2cccef8975d8520a5df39a4e2821cf40f1a8d6a0
João Adriano <joao@nortech.ai>
2022-09-20 12:32:53 +0100

    Previous assumptions about docker were incorrect. Image platform is not the same as build platform

--- build.ts
+++ build.ts
@@ -1,64 +1,88 @@
 #!/usr/bin/env ts-node --swc
 
-import { exec } from "child_process"
-import { promisify } from "util"
+import { exec } from "child_process";
+import { promisify } from "util";
 
-const _exec = promisify(exec)
+const _exec = promisify(exec);
 
-const IMAGE_NAME = "joaonortech/rust-multiarch"
+const IMAGE_NAME = "joaonortech/rust-multiarch";
 
 const ARCHS = [
   {
     name: "amd64",
-    docker: "amd64",
-    platform: "linux/amd64",
+
+    finalName: `${IMAGE_NAME}:latest-amd64`,
   },
   {
-    name: "arm64v8",
-    docker: "arm64",
-    platform: "linux/arm64",
+    name: "arm64",
+    finalName: `${IMAGE_NAME}:latest-arm64`,
   },
   {
     name: "arm32v7",
-    docker: "arm",
-    platform: "linux/arm/v7",
+    finalName: `${IMAGE_NAME}:latest-arm32v7`,
   },
-]
+];
+
+const USER_ARCHS = [
+  {
+    buildName: buildAmd64Name,
+    name: "amd64",
+    platform: "linux/amd64",
+  },
+  {
+    name: "arm64",
+    buildName: buildArm64Name,
+    platform: "linux/arm64",
+  },
+];
 
 async function main() {
   const builds = ARCHS.map((arch) => async () => {
-    console.log(`BUILD ${arch.name}`)
-    const preName = `${IMAGE_NAME}:${arch.name}`
-    await $(
-      `docker buildx build --push --platform ${arch.platform} -t ${preName} -f Dockerfile.${arch.name} .`
-    )
-  })
+    console.log(`BUILD ${arch.name}`);
+    for (const userArch of USER_ARCHS) {
+      await $(
+        `docker buildx build --push --platform ${
+          userArch.platform
+        } -t ${userArch.buildName(arch)} -f Dockerfile.${arch.name}-${
+          userArch.name
+        } .`
+      );
+    }
+  });
 
-  await Promise.all(builds.map((build) => build()))
+  await Promise.all(builds.map((build) => build()));
 
-  const finalName = `${IMAGE_NAME}:latest`
-  await $(`docker manifest rm ${finalName}`)
-  await $(`docker manifest create ${finalName} \
-    ${ARCHS.map((arch) => `${`${IMAGE_NAME}:${arch.name}`}`).join(" ")}
-  `)
+  for (const arch of ARCHS) {
+    try {
+      await $(`docker manifest rm ${arch.finalName}`);
+    } catch (error) {}
 
-  const annotates = ARCHS.map((arch) => async () => {
-    await $(
-      `docker manifest annotate "${finalName}" ${`${IMAGE_NAME}:${arch.name}`} --arch ${
-        arch.docker
-      }`
-    )
-  })
-  await Promise.all(annotates.map((annotate) => annotate()))
-  await $(`docker manifest push "${IMAGE_NAME}:latest"`)
-  await $(`docker manifest inspect "${IMAGE_NAME}:latest"`)
+    await $(`docker manifest create ${arch.finalName} \\
+      ${USER_ARCHS.map((userArch) => userArch.buildName(arch)).join(" ")}
+    `);
+
+    for (const userArch of USER_ARCHS) {
+      await $(`docker manifest annotate ${arch.finalName} \\
+        ${userArch.buildName(arch)} --os linux --arch ${userArch.name}
+      `);
+    }
+    await $(`docker manifest push ${arch.finalName}`);
+  }
 }
 
-main()
+main();
+
+function buildArm64Name(arch: { name: string }) {
+  return `${IMAGE_NAME}:build-${arch.name}-from-arm64`;
+}
+
+function buildAmd64Name(arch: { name: string }) {
+  return `${IMAGE_NAME}:build-${arch.name}-from-amd64`;
+}
 
 async function $(cmd: string) {
-  console.log(`$ ${cmd}`)
-  const com = await _exec(cmd, {})
-  console.log(com.stderr)
-  console.log(com.stdout)
+  console.log(`$ ${cmd}`);
+  const com = await _exec(cmd, {});
+  console.log(com.stderr);
+  console.log(com.stdout);
 }
